7bb3565540c7cf74139951c3803ccd31777aa4f7
bgb051004-coder <bgb051004@gmail.com>
2026-01-20 08:29:43 +0700

    Core Components - Buoi 2

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Text style={styles.title}>Đăng nhập</Text>
+
+     
+      <Text style={styles.label}>Nhập số điện thoại</Text>
+
+      
+      <Text style={styles.description}>
+        Dùng số điện thoại để đăng nhập hoặc đăng ký tài khoản tại OneHousing Pro
+      </Text>
+
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Nhập số điện thoại của bạn"
+        keyboardType="phone-pad"
+      />
+
+      
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Tiếp tục</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +36,46 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 20,
+  },
+
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 40,
+    marginBottom: 30,
+  },
+
+  label: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginBottom: 8,
+  },
+
+  description: {
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 20,
+  },
+
+  input: {
+    height: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    fontSize: 16,
+    marginBottom: 40,
+  },
+
+  button: {
+    height: 50,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 8,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontSize: 16,
+    color: '#999',
   },
 });
